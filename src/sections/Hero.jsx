303e14b71590ce4f8fa0d0479c6e69eb00f866cc
Din Muhammad Rezwoan <frezwoan@gmail.com>
2025-08-01 22:11:00 +0600

    arrow fixex

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,41 +13,40 @@ const Hero = () => {
                 <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
                     <div className="flex flex-col gap-7">
                         <div className="hero-text">
-                            <h1 className="">
+                            <h1>
                                 Shaping
                                 <span className="slide">
                                     <span className="wrapper">
-                                        {words.map((word) => (
+                                        {words.map((word, index) => (
                                             <span
-                                                key={word.text}
+                                                key={index}
                                                 className="flex items-center md:gap-3 gap-1 pb-2"
                                             >
                                                 <img
                                                     src={word.imgPath}
-                                                    alt={word.text}
-                                                    className="inline-block xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50 "
+                                                    alt="person"
+                                                    className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
                                                 />
-                                                <span className="text">
-                                                    {word.text}
-                                                </span>
+                                                <span>{word.text}</span>
                                             </span>
                                         ))}
                                     </span>
                                 </span>
                             </h1>
-
-                            <h1 className="">into Real Project</h1>
-                            <h1 className="">that Deliver Results</h1>
-                            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-                                Hi, I'm Rezwoan, a developer based in Bangladesh
-                                with a passion for CODE
-                            </p>
-                            <Button
-                                className="md:w-80 md:h-16 w-60 h-12"
-                                id="button"
-                                text="See my Work"
-                            />
+                            <h1>into Real Projects</h1>
+                            <h1>that Deliver Results</h1>
                         </div>
+
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                            Hi, I’m Adrian, a developer based in Croatia with a
+                            passion for code.
+                        </p>
+
+                        <Button
+                            text="See My Work"
+                            className="md:w-80 md:h-16 w-60 h-12"
+                            id="counter"
+                        />
                     </div>
                 </header>
             </div>
